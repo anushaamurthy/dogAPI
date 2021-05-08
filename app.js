e07bd7e782d7008dbf074ace32bec93ccd38c8ce
Anusha A <anushamurthy201@gmail.com>
2021-05-09 00:36:59 +0530

    added fetch code to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('hello')
+const data =(fetch("https://dog.ceo/api/breeds/image/random"))
+            .then(function(response) {
+            return response.json()
+            }).then(function(jsonData) {
+              console.log(jsonData)
+            })
